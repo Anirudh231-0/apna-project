aefd066b28eca3151cccfed14f6cd7cd10c9df84
Anirudh231-0 <anirudhpisharady@gmail.com>
2026-01-12 21:55:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function login() {
+    // Temporary redirect (later connect to backend)
+    window.location.href = "dashboard.html";
+}
+
+function transfer(){
+    window.location.href="transfer.html";
+}
+
+function transaction(){
+    window.location.href="transactions.html";
+}
